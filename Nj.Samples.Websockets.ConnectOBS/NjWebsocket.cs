383962357e89594099edbd99225d4378e6bc557b
samuel.maicas.development <samuel.maicas.development@gmail.com>
2023-04-30 16:29:57 +0200

    Add sample websockets

---
+++ Nj.Samples.Websockets.ConnectOBS/NjWebsocket.cs
@@ -0,0 +1,59 @@
+﻿using System.Net.WebSockets;
+
+namespace Nj.Core.Websocket.Extensions;
+public class NjWebsocket : IDisposable
+{
+
+    private System.Net.WebSockets.WebSocket? _webSocket;
+    private readonly Func<Uri, CancellationToken, Task<WebSocket>> _connectionFactory;
+
+    public NjWebsocket() =>
+        _connectionFactory = (Func<Uri, CancellationToken, Task<WebSocket>>)(async (uri, token) =>
+        {
+            ClientWebSocket client = new ClientWebSocket();
+            await client.ConnectAsync(uri, token).ConfigureAwait(false);
+            WebSocket webSocket = (WebSocket)client;
+            return webSocket;
+        });
+
+    public async Task ConnectAsync(Uri uri, CancellationToken ct = default)
+    {
+        _webSocket = await _connectionFactory(uri, ct);
+    }
+
+    public async Task SendAsync(string message, CancellationToken cancellationToken = default)
+    {
+        var buffer = System.Text.Encoding.UTF8.GetBytes(message);
+        if (_webSocket != null)
+            await (_webSocket.SendAsync(new ArraySegment<byte>(buffer),
+                WebSocketMessageType.Text,
+                true,
+                cancellationToken)).ConfigureAwait(false);
+    }
+
+    public async Task<string> ReceiveAsync(CancellationToken cancellationToken = default)
+    {
+        var buffer = new byte[1024];
+        if (_webSocket != null)
+        {
+            var result = await _webSocket.ReceiveAsync(new ArraySegment<byte>(buffer), cancellationToken);
+
+            if (result.MessageType != WebSocketMessageType.Text)
+            {
+                throw new InvalidOperationException("Unexpected websocket message type.");
+            }
+
+            var message = System.Text.Encoding.UTF8.GetString(buffer, 0, result.Count);
+
+            return message;
+        }
+
+        return string.Empty;
+    }
+
+    /// <inheritdoc />
+    public void Dispose()
+    {
+        _webSocket?.Dispose();
+    }
+}
--- Nj.Samples.Websockets.ConnectOBS/Program.cs
+++ Nj.Samples.Websockets.ConnectOBS/Program.cs
@@ -1,15 +1,17 @@
-﻿CancellationTokenSource _cts = new CancellationTokenSource();
+﻿using Nj.Core.Websocket.Extensions;
+
+CancellationTokenSource _cts = new CancellationTokenSource();
 var password = "NILWXLbPUHrzO8pm";
-var uri = new Uri($"obsws://192.168.1.38:4455/{password}");
-ObsWebsocketClient _socket = new ObsWebsocketClient(uri);
+var uri = new Uri($"ws://192.168.1.38:4455");
+NjWebsocket socket = new();
 
 _cts = new CancellationTokenSource();
 
-await _socket.ConnectAsync(_cts.Token);
+await socket.ConnectAsync(uri);
+var res = await socket.ReceiveAsync();
 
 // The connection has been established
 Console.WriteLine("Connection to OBS Studio established!");
 
-while (!(Console.ReadLine() == "exit")) { }
-
-_socket.Dispose();
+while (Console.ReadLine() != "exit") { }
+socket.Dispose();
