a0b3a1eddb16da1d2e631a2e949a6a4b4c8e9910
samuel.maicas.development <samuel.maicas.development@gmail.com>
2023-04-30 13:24:16 +0200

    Init

---
+++ Nj.Samples.Websockets.ConnectOBS/Program.cs
@@ -0,0 +1,15 @@
+﻿CancellationTokenSource _cts = new CancellationTokenSource();
+var password = "NILWXLbPUHrzO8pm";
+var uri = new Uri($"obsws://192.168.1.38:4455/{password}");
+ObsWebsocketClient _socket = new ObsWebsocketClient(uri);
+
+_cts = new CancellationTokenSource();
+
+await _socket.ConnectAsync(_cts.Token);
+
+// The connection has been established
+Console.WriteLine("Connection to OBS Studio established!");
+
+while (!(Console.ReadLine() == "exit")) { }
+
+_socket.Dispose();
